731dbc760d3a6a5074319729926392199f3ebb7f
vanshdeep signh <vanshdeep0706@gmail.com>
2026-07-10 23:26:14 +0530

    Fix dotenv and connection variable

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const path = require("path")
 const qr=require("qrcode")
-require("dotenv").config();
+//require("dotenv").config();
 const app = express();
 const port = 8003;
 
